803cf892c39d703046af3d6d123956b9dfc1b79a
Iksoon <ik9702@gmail.com>
2022-02-03 01:24:15 +0900

    난몰라

--- hmm.py
+++ hmm.py
@@ -1,26 +1,10 @@
-import numpy as np
+import os
 import torch
-import numpy as np
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
 
-print("hmm")
 
-data = [[1,2],[3,4]]
-x_data = torch.tensor(data)
-
-np_array = np.array(data)
-x_np = torch.from_numpy(np_array)
-
-shape = (2,2,)
-
-x_ones = torch.ones_like(x_data)
-x_rand = torch.rand_like(x_data, dtype=torch.float)
-
-print(x_ones, x_rand)
-print(x_rand.shape)
-print(x_ones.device)
-
-if torch.cuda.is_available():
-    x_ones = x_ones.to('cuda')
-
-print(x_ones.device)
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# print(f'Using {device} device')
