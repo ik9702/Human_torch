6af00ee7ebfdd61d41998904582346d195137b75
Iksoon <ik9702@gmail.com>
2022-02-04 03:23:12 +0900

    22/2/4

--- con2d_practice.py
+++ con2d_practice.py
@@ -2,43 +2,32 @@ import torch
 import numpy as np
 
 
+torch.nn.Conv2d(
+    in_channels,            #입력 데이터의 채널 수    
+    out_channels,           #출력 데이터의 채널 수
+    kernel_size,            #filter 크기
+    stride=1,               #합성곱시 필터가 움직이는 간격
+    padding=0,              #input을 둘러싸는 padding의 수
+    dilation=1,             #필터 사이 간격을 의미
+    groups=1,               #input channel과 output channel 대응 설정
+    bias=True,              #output channel의 가중치 적용 여부
+    padding_mode='zeros'    #input을 둘러싸는 값 설정
+)
 
 
 
+m1 = torch.nn.Conv2d(16, 33, 3, stride=2)
+#3*3 필터, stride=(2,2)
 
+m2 = torch.nn.Conv2d(16,33,(3,5),stride=2,padding=(4,2))
+#3*5 필터, stride=2, padding(4,2)
 
-
-
-
-
-
-
-# torch.nn.Conv2d(
-#     in_channels, 
-#     out_channels, 
-#     kernel_size, 
-#     stride=1, 
-#     padding=0, 
-#     dilation=1,     #atrous 알고리즘, 필터 사이 간격을 의미
-#     groups=1,       #1:일반적인 합성곱, 2:2그룹으로 나누어 연산 후 결합(연쇄), 3ㅑㅜ_초무ㅜ딘:각 ㅇ니풋 채널이 아웃풋 채널과 대응되어 연산(size:out_channels//in_channel)
-#     bias=True, 
-#     padding_mode='zeros'
-# )
-
-
-
-m1 = torch.nn.Conv2d(16, 1, 3, stride=2)
-
-m2 = torch.nn.Conv2d(16,1,(3,5),stride=2,padding=(4,2))
-
-m3 = torch.nn.Conv2d(16,1,(3,5),stride=(2,1),padding=(4,2), dilation=(3,1))
-
+m3 = torch.nn.Conv2d(16,33,(3,5),stride=(2,1),padding=(4,2), dilation=(3,1))
+#3*5 필터, stride (2,1), paddint(4,2), dilation (3,1)
 
 input = torch.randn(20,16,50,100)
 
 output = m1(input)
 
 print(input, m1, m2, m3, output)
-
-
 
